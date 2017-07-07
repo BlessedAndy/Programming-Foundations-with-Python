c8bea62da3583b06823dbe90529bcc6f392aa9ee
Andy <shumaozhang@yahoo.com>
2017-07-07 23:54:58 +0700

    inheritance

--- Programming Foundations with Python/src/cn/careerwinner/udacity/inheritance.py
+++ Programming Foundations with Python/src/cn/careerwinner/udacity/inheritance.py
@@ -10,7 +10,11 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
         
-class Child():
+    def print_info(self):
+        print("Last Name: " + self.last_name)
+        print("Eye Color: " + self.eye_color)
+        
+class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child Constructor Called")
         Parent.__init__(self, last_name, eye_color)
@@ -23,3 +27,4 @@ miley_cyrus = Child("Cyrus","Blue",5)
    
 print(miley_cyrus.last_name)
 print(miley_cyrus.number_of_toys)
+miley_cyrus.print_info()
