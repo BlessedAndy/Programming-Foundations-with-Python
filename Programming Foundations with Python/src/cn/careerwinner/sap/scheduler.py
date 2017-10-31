01579a04b4639248ba2e75ed451938bb60c6c6f8
Andy Zhang <shumaozhang@yahoo.com>
2017-10-30 21:59:15 -0600

    make scheduler run only one time everyday.

--- Programming Foundations with Python/src/cn/careerwinner/sap/scheduler.py
+++ Programming Foundations with Python/src/cn/careerwinner/sap/scheduler.py
@@ -9,6 +9,7 @@ import time
 import callExport
 import util
 from datetime import date
+from asyncio.tasks import sleep
 
 def exportReport():
     #Open it only when require a full load
@@ -29,28 +30,32 @@ print('Please close remote desktop with - leave remote desktop - bat file')
 #                                                                           #
 #############################################################################
 
+todayDate = str(date.today().day)
+
 while True:
     schedule.run_pending()
     #sleep 5 MIN, you can change the time here to change the check rate
     time.sleep(60*5)
     
-    todayDate = str(date.today().day)
+    newDay = str(date.today().day)
 
     #Daily job, only skip 23
     
-    if(todayDate != '23'):
+    if(todayDate != newDay & newDay != '23'):
+        print('begin to exporting report and check files')
         schedule.every().day.at("01:12").do(exportReport)
         # schedule for check Report files
         schedule.every().day.at("03:12").do(checkFiles)
-        print('begin to exporting report and check files')
+        print('finished the export and check files.')
+        todayDate = newDay
     
     #Monthly Job
-    if(todayDate == '23'):
+    if(todayDate != newDay & newDay == '23'):
+        print('begin to exporting report and check files')
         # schedule for export report
         schedule.every().day.at("01:12").do(exportReport)
         # schedule for check Report files
         schedule.every().day.at("03:12").do(checkFiles)
-        print('begin to exporting report and check files')
-
-
-    
+        print('finished the export and check files.')
+        todayDate = newDay
+   
