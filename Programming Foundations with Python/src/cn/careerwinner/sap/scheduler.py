a6e80e804fcc4bd1a8a57d1172689e31261fdd5d
Andy Zhang <shumaozhang@yahoo.com>
2017-11-16 14:47:18 -0600

    debug and update the scheduler

--- Programming Foundations with Python/src/cn/careerwinner/sap/scheduler.py
+++ Programming Foundations with Python/src/cn/careerwinner/sap/scheduler.py
@@ -29,7 +29,7 @@ print('Please close remote desktop with - leave remote desktop - bat file')
 #                                                                           #
 #############################################################################
 
-todayDate = str(date.today().day)
+todayDate = str(0)
 
 while True:
     schedule.run_pending()
@@ -37,25 +37,26 @@ while True:
     newDay = str(date.today().day)
     print('New Day: ' + newDay)
 
-    #sleep 5 MIN, you can change the time here to change the check rate
-    time.sleep(60*5)
-    
     #Daily job, only skip 23
-    if(todayDate != newDay and newDay != '15'):
+    if(todayDate != newDay and newDay != '23'):
         print('begin to exporting report and check files')
-        schedule.every().day.at("01:12").do(exportReport)
+#         schedule.every().day.at("01:12").do(exportReport)
+        print('ENTERED THE EVERYDAY EXCEPT SPECIFIC DAY ...');
         # schedule for check Report files
-        schedule.every().day.at("03:12").do(checkFiles)
-        print('finished the export and check files.')
+#         schedule.every().day.at("03:12").do(checkFiles)
+#         print('finished the export and check files.')
         todayDate = newDay
     
     #Monthly Job
-    if(todayDate != newDay and newDay == '15'):
+    if(todayDate != newDay and newDay == '23'):
         print('begin to exporting report and check files')
         # schedule for export report
-        schedule.every().day.at("01:12").do(exportReport)
+#         schedule.every().day.at("01:12").do(exportReport)
+        print('ENTERED THE SPECIFIC DAY ...');
         # schedule for check Report files
-        schedule.every().day.at("03:12").do(checkFiles)
+#         schedule.every().day.at("03:12").do(checkFiles)
         print('finished the export and check files.')
         todayDate = newDay
-   
+    
+    #sleep 5 MIN, you can change the time here to change the check rate
+    time.sleep(60*5)
